98c7140c50593f5e99f00175cc7cb904e382785c
galperins4 <galperins4@gmail.com>
2021-07-05 16:49:17 -0500

    Update initialize.py

--- core/modules/initialize.py
+++ core/modules/initialize.py
@@ -1,3 +1,22 @@
+import os
+from pathlib import Path
+
 class Initialize:
     def __init__(self):
+        self.home = str(Path.home())
+        data_path = self.home+'/core3-tbw/core/data/tbw.db'
+        
+        if os.path.exists(data_path) == False:
+            print("Setting up database")
+            self.initialize()
+            print("Finished setting up database")
+            quit()
+            
+        else:
+            print("Database detected - no initialization needed")
+    
+    def initialize(self):
         pass
+
+    
+    
