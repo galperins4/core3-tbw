5a3ab83328cf6850e742e1eb0efd81a20c95b438
galperins4 <galperins4@gmail.com>
2021-07-05 16:39:21 -0500

    Update utility.py

--- core/utility/utility.py
+++ core/utility/utility.py
@@ -1,1 +1,10 @@
-#placeholder
+from client import ArkClient
+
+
+class Utility:
+    def __init__(self, network):
+        self.network = network
+    
+    
+    def get_client(self, ip="localhost"):
+        return ArkClient('http://{0}:{1}/api'.format(ip, self.network.api))
