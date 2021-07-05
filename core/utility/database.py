69de7920432f2d48752fe278eab4686351efd6a5
galperins4 <galperins4@gmail.com>
2021-07-05 14:12:00 -0500

    Update database.py

--- core/utility/database.py
+++ core/utility/database.py
@@ -34,7 +34,7 @@ class Database:
         for i in universe:
             for k,v in i[1].items():
                 if k == 'delegate' and v['username']=='genesis_1':
-                    self.publickey = k
+                    self.publickey = i[0]
     
     def get_current_nonce(self):
         pass
