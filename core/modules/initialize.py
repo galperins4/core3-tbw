877d6340a6efb4e58161ca0a00d95fcad2f97699
galperins4 <galperins4@gmail.com>
2021-07-05 17:02:08 -0500

    Update initialize.py

--- core/modules/initialize.py
+++ core/modules/initialize.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 class Initialize:
-    def __init__(self, database):
+    def __init__(self, database, sql):
         self.home = str(Path.home())
         self.database = database
         data_path = self.home+'/core3-tbw/core/data/tbw.db'
