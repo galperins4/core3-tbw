4fb8649a3771284414daee494835ee4a2443fbe6
galperins4 <galperins4@gmail.com>
2021-07-05 12:43:46 -0500

    Update configure.py

--- core/config/configure.py
+++ core/config/configure.py
@@ -4,24 +4,24 @@ class Configure:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        self.static_config()
-        self.delegate_config()
-        self.payment_config()
-        self.experimental_config()
+        self.static(config)
+        self.delegate(config)
+        self.payment(config)
+        self.experimental(config)
         
         
-    def static_config(self):
+    def static(self, config):
         self.atomic = config.get('static', 'atomic')
         print(atomic)
     
     
-    def delegate_config(self):
+    def delegate(self):
         pass
     
     
-    def payment_config(self):
+    def payment(self):
         pass
     
     
-    def experimental_config(self):
+    def experimental(self):
         pass
