7ddd36c3944c209c38c689432878614b00b7c34b
galperins4 <galperins4@gmail.com>
2021-07-05 12:34:48 -0500

    Update configure.py

--- core/config/configure.py
+++ core/config/configure.py
@@ -3,5 +3,5 @@ import configparser
 class Configure:
     def __init__(self):
         config = configparser.ConfigParser()
-        config.read('~\core3-tbw\core\config\config.ini')
+        config.read('config.ini')
         print(config.sections())
