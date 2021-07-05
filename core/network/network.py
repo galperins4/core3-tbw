60c72536c0c2f1ed01b91351d56fbc8ab47cb711
galperins4 <galperins4@gmail.com>
2021-07-05 12:54:02 -0500

    Update network.py

--- core/network/network.py
+++ core/network/network.py
@@ -1,7 +1,11 @@
 import configparser
+from pathlib import Path
 
 class Network:
     def __init__(self):
+        #self.home = str(Path.home())
+        #env_path = self.home+'/core3-tbw/core/network/'+self.network
+        
         #config = configparser.ConfigParser()
         #config.read('\config\config.ini')
         pass
