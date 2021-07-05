5a8cbefc8584780bc4507bfb57f41f6780dde3a5
galperins4 <galperins4@gmail.com>
2021-07-05 13:26:12 -0500

    Update database.py

--- core/utility/database.py
+++ core/utility/database.py
@@ -2,5 +2,31 @@ import psycopg2
 
 
 class Database:
-    def __init__(self):
-      pass
+    def __init__(self, config, network):
+        print(network.database)
+        print(config.user)
+        print(network.password)
+    
+    
+    
+    def __init__(self, db, u, pw, pk):
+        self.db=db
+        self.user=u
+        self.password=pw
+        self.PublicKey=pk
+    
+    
+    def open_connection(self):
+        self.connection = psycopg2.connect(
+            dbname = self.db,
+            user = self.user,
+            password= self.password,
+            host='localhost',
+            port='5432')
+            
+        self.cursor=self.connection.cursor()
+    
+    
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
