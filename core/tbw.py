20c26dfea361ac1dd5b8caee3085c0cb5982518a
galperins4 <galperins4@gmail.com>
2021-07-05 12:01:34 -0500

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+from modules import Allocate
+from modules import Blocks
+from modules import Configure
+from modules import Initialize
+from modules import Payments
+from modules import Stage
+from modules import Voters
 
 
 if __name__ == '__main__':
