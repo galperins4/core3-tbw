9ff7552104388880cee6f0db0dcc12d20f5c1a21
galperins4 <galperins4@gmail.com>
2021-07-05 14:11:19 -0500

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
     database = Database(config, network)
     database.open_connection()
     database.get_publickey()
+    print(database.publickey)
     database.close_connection()
     
     
