a8a61ebfb2a5ee6257333344d955bbbbbcf82c57
galperins4 <galperins4@gmail.com>
2021-07-07 14:07:32 -0500

    Update initialize.py

--- core/modules/initialize.py
+++ core/modules/initialize.py
@@ -14,13 +14,17 @@ class Initialize:
             self.initialize()
             
             print("Importing forged blocks")
-            
+            self.database.open_connection()
+            total_blocks = self.database.get_all_blocks()
+            self.database.close_connection()
             
             print("Storing forged blocks in database")
-            
+            self.sql.open_connection()
+            self.sql.store_blocks(total_blocks)
             
             print("Marking blocks proccessed up to starting block")
-            
+            self.sql.mark_processed(self.config.start_block, initial = "Y")
+            self.sql.close_connection()
             
             print("Finished setting up database")
             
