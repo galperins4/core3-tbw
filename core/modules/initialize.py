d79b639f57f74b0f71d77b6e247a1318939db8ec
galperins4 <galperins4@gmail.com>
2021-07-07 13:46:58 -0500

    Update initialize.py

--- core/modules/initialize.py
+++ core/modules/initialize.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
-from utility.sql import Sql
 
 class Initialize:
-    def __init__(self, config, database):
+    def __init__(self, config, database, sql):
         self.home = str(Path.home())
         self.database = database
+        self.sql = sql
         self.config = config
         data_path = self.home+'/core3-tbw/core/data/tbw.db'
         
@@ -18,10 +18,9 @@ class Initialize:
             print("Database detected - no initialization needed")
     
     def initialize(self):
-        sql = Sql()
-        sql.open_connection()
-        sql.setup()
-        sql.close_connection()
+        self.sql.open_connection()
+        self.sql.setup()
+        self.sql.close_connection()
         quit()
 
     
