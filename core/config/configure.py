eeba978291091fd53fb54376ef9477525bb9c385
galperins4 <galperins4@gmail.com>
2021-07-07 12:20:17 -0500

    Update configure.py

--- core/config/configure.py
+++ core/config/configure.py
@@ -18,14 +18,25 @@ class Configure:
         self.atomic = int(c.get('static', 'atomic'))
         self.network = c.get('static', 'network')
         self.username = c.get('static', 'username')
+        self.block_check = int(c.get('static', 'block_check'))
+        self.start_block = int(c.get('static', 'start_block'))
     
     
     def delegate(self, c):
         self.delegate = c.get('delegate', 'delegate')
+        self.message = c.get('delegate', 'message')
+        self.voter_share = c.get('delegate', 'voter_share')
+        self.voter_cap = c.get('delegate', 'voter_cap')
+        self.voter_min = c.get('delegate', 'voter_min')
+        self.fixed = c.get('delegate', 'fixed')
+        self.whitelist = c.get('delegate', 'whitelist')
+        self.whitelist_address = c.get('delegate', 'whitelist_address')
+        self.blacklist = c.get('delegate', 'blacklist')
+        self.blacklist_address = c.get('delegate', 'blacklist_address')
     
     
     def payment(self, c):
-        pass
+        self.interval = c.get('payment', 'interval')
     
     
     def experimental(self, c):
