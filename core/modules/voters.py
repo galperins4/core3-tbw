a697865637e056c17c69afecfd0946780334fe4b
galperins4 <galperins4@gmail.com>
2021-11-21 17:48:52 -0600

    Update voters.py

--- core/modules/voters.py
+++ core/modules/voters.py
@@ -3,3 +3,24 @@ from utility.utility import Utility
 class Voters():
     def __init__(self, config, sql):
         pass
+
+def create_voter_roll(self, v, u):
+    #create dictionary of unvotes
+    unvotes = {i[0]:i[1] for i in u}
+
+    roll = []
+
+    for i in v:
+        address = i[0]
+        val = [address_from_public_key(address), address]
+        if address in unvotes.keys():
+            vote_ts = i[1]
+            unvote_ts =  unvotes[address]
+
+            # check to see if unvote is prior to vote
+            if vote_ts > unvote_ts:
+                roll.append(val)
+        else:
+            roll.append(val)
+
+    return roll
