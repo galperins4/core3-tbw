de67875abcd195bd3140081d0dfc2d1cbb440115
galperins4 <galperins4@gmail.com>
2021-11-21 19:44:33 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -16,35 +16,34 @@ if __name__ == '__main__':
     print("Start Script")
     # get configuration
     config = Configure()
-    #load network
+    
+    # load network
     network = Network(config.network)
-    #load utility
+    
+    # load utility
     utility = Utility(network)
+    
     # connect to core and tbw script database
     database = Database(config, network)
     sql = Sql()
+    
     # check if initialized
     Initialize(config, database, sql)
-    # process blocks
+    
+    # get blocks
     block = Blocks(config, database, sql)
-    #sql.open_connection()
-    #database.open_connection()
+    
     # get last block to start
     last_block = block.get_last_block()
-    # last_block = sql.last_block().fetchall()
+    
     # use last block timestamp to get all new blocks
     new_blocks = block.get_new_blocks(last_block)
-    # new_blocks = database.get_limit_blocks(last_block[0][0])
+    
     # store all new blocks
     block.store_new_blocks(new_blocks)
-    # sql.store_blocks(new_blocks)
+    
     # get unprocessed blocks
     unprocessed_blocks = block.return_unprocessed_blocks()
-    # unprocessed_blocks = sql.unprocessed_blocks().fetchall()
-    print(len(unprocessed_blocks))
-    
-    #sql.close_connection()
-    #database.close_connection()    
     
     quit()
     # allocate block rewards
