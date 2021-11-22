471ef74d1c53bb31cab4b552c92a6853ab620fb8
galperins4 <galperins4@gmail.com>
2021-11-21 19:12:32 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -29,8 +29,12 @@ if __name__ == '__main__':
     # Blocks(config, database, sql)
     sql.open_connection()
     database.open_connection()
+    # get last block to start
     last_block = sql.last_block().fetchall()
+    # use last block timestamp to get all new blocks
     new_blocks = database.get_limit_blocks(last_block[0][0])
+    # store all new blocks
+    database.store_blocks(new_blocks)
     
     
     sql.close_connection()
