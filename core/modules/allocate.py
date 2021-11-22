f56140823c4f9b3cc92f61fbc674f7f143706e09
galperins4 <galperins4@gmail.com>
2021-11-21 20:19:24 -0600

    Update allocate.py

--- core/modules/allocate.py
+++ core/modules/allocate.py
@@ -12,6 +12,33 @@ class Allocate:
         return vote, unvote    
 
     
+    def create_voter_roll(self, v, u):
+        #create dictionary of unvotes
+        unvotes = {i[0]:i[1] for i in u}
+
+        roll = []
+
+        for i in v:
+            address = i[0]
+            val = [address_from_public_key(address), address]
+            if address in unvotes.keys():
+                vote_ts = i[1]
+                unvote_ts =  unvotes[address]
+
+                # check to see if unvote is prior to vote
+                if vote_ts > unvote_ts:
+                    roll.append(val)
+            else:
+                roll.append(val)
+
+        return roll
+    
+    
+    
+    
+    
+    
+    
     def get_block_allocations(self, block, voters):
         rewards_check = 0
         voter_check = 0
@@ -50,26 +77,7 @@ class Allocate:
         Total Block Rewards: {total_reward}""")
 
     
-    def create_voter_roll(self, v, u):
-        #create dictionary of unvotes
-        unvotes = {i[0]:i[1] for i in u}
-
-        roll = []
-
-        for i in v:
-            address = i[0]
-            val = [address_from_public_key(address), address]
-            if address in unvotes.keys():
-                vote_ts = i[1]
-                unvote_ts =  unvotes[address]
-
-                # check to see if unvote is prior to vote
-                if vote_ts > unvote_ts:
-                    roll.append(val)
-            else:
-                roll.append(val)
-
-        return roll
+    
 
 
     def get_voter_balance(self, block):
