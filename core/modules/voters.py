8cadd549a7e8ec0feb52aba96a091e359d5ed3f3
galperins4 <galperins4@gmail.com>
2021-11-21 18:00:45 -0600

    Update voters.py

--- core/modules/voters.py
+++ core/modules/voters.py
@@ -24,3 +24,27 @@ def create_voter_roll(self, v, u):
             roll.append(val)
 
     return roll
+
+
+def get_voter_balance(self, block):
+    vote_balance = {}
+    block_timestamp = block[1]
+    vote_tx, unvote_tx = get_votes(delegate, block_timestamp)
+    voter_roll = create_voter_roll(vote_tx, unvote_tx)
+    
+    print("Current Voters: ", len(voter_roll))
+    print("As of block id: ", block[0])
+    print("Delegate public key: ", delegate)
+
+    for i in voter_roll:
+        # print(i)
+        debit = get_sum_outbound(i[1], block_timestamp)
+        credit = get_sum_inbound(i[0], block_timestamp)
+        balance = credit - debit
+        vote_balance[i[0]] = balance
+
+    print("Block Reward: ", block[2])
+    print("Block Fees: ", block[3])
+    print("Total Allocation: ", block[2]+block[3])
+    print("Total Voter Balances: ", sum(vote_balance.values()), "\n")
+    return vote_balance
