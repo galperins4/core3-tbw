3c17134b6507a56dcea51b09eab5c204dd8eb2e7
galperins4 <galperins4@gmail.com>
2021-11-25 17:56:35 -0600

    Update sql.py

--- core/utility/sql.py
+++ core/utility/sql.py
@@ -211,8 +211,8 @@ class Sql:
 
     def update_voter_paid_balance (self, paid):
         for k, v in paid.items():
-            self.cursor.execute(f"UPDATE voters SET paid_bal = paid_bal + unpaid_bal WHERE address = '{k}'")
-            self.cursor.execute(f"UPDATE voters SET unpaid_bal = unpaid_bal - unpaid_bal WHERE address = '{k}'")
+            self.cursor.execute(f"UPDATE voters SET paid_bal = paid_bal + {v} WHERE address = '{k}'")
+            self.cursor.execute(f"UPDATE voters SET unpaid_bal = unpaid_bal - {v} WHERE address = '{k}'")
             self.commit()
 
 
