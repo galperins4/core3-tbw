0349893aabb59621153561a0b16032561aafb8ed
galperins4 <galperins4@gmail.com>
2021-11-25 13:07:30 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -59,7 +59,8 @@ if __name__ == '__main__':
         voter_roll = allocate.create_voter_roll(vote, unvote)
         # get voter_balances
         voter_balances = allocate.get_voter_balance(unprocessed, voter_roll)
-        print(voter_balances)
+        for i in voter_balances:
+            print(i)
         quit()
         # run voters through various vote_options
         if config.whitelist == 'Y':
