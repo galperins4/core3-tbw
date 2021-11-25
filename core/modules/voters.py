482c2e6455e133e986e7d329f4e964156c219bf8
galperins4 <galperins4@gmail.com>
2021-11-25 13:14:19 -0600

    Update voters.py

--- core/modules/voters.py
+++ core/modules/voters.py
@@ -5,3 +5,21 @@ class Voters():
         self.config = config
         self.sql = sql
 
+    def process_whitelist(self, voter_balances):
+        pass
+
+    
+    def process_blacklist(self, voter_balances):
+        pass
+    
+    
+    def process_voter_cap(self, voter_balances):
+        pass
+    
+    
+    def process_voter_min(self, voter_balances):
+        pass
+    
+    
+    def process_anti_dilution(self, voter_balances):
+        pass
