3ef67578da739f74511b630a96b07cd05ecc6b54
galperins4 <galperins4@gmail.com>
2021-11-25 16:41:41 -0600

    Update stage.py

--- core/modules/stage.py
+++ core/modules/stage.py
@@ -1,6 +1,19 @@
 class Stage:
-    def __init__(self, config, utility, voters, delegate):
+    def __init__(self, config, utility, sql, voters, delegate):
         self.config = config
         self.utility = utility
+        self.sql = sql
         self.voters = voters
         self.delegate = delegate
+        
+        
+    def get_transaction_fees(self):
+        pass
+    
+    
+    stage_delegate_payments(self):
+        pass
+    
+    
+    stage_voter_payments(self):
+        pass
