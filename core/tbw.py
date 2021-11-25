b32ae780a0b3fb525253c6bc9db2379e73ddbd90
galperins4 <galperins4@gmail.com>
2021-11-25 16:41:53 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
         # check if true to stage payments
         if stage:
             print("\nStaging payments")
-            s = Stage(config, utility, unpaid_voters, unpaid_delegate)
+            s = Stage(config, utility, sql, unpaid_voters, unpaid_delegate)
         
         
         # check result and stage payments (if necessary)
