891c7dd59aa6eba66b26cd356865b2c0e3e5156f
galperins4 <galperins4@gmail.com>
2021-11-25 17:05:57 -0600

    Update stage.py

--- core/modules/stage.py
+++ core/modules/stage.py
@@ -1,7 +1,7 @@
 class Stage:
-    def __init__(self, config, utility, sql, voters, delegate):
+    def __init__(self, config, dynamic, sql, voters, delegate):
         self.config = config
-        self.utility = utility
+        self.dynamic = dynamic
         self.sql = sql
         self.voters = voters
         self.delegate = delegate
@@ -19,8 +19,23 @@ class Stage:
         voter_tx = len([v for v in self.voters.values() if v > 0])
         total_tx = voter_tx + delegate_tx
         print(total_tx)
-        quit()
-    
+        
+        # check if multipayments
+        if self.config.multi == "Y":
+            multi_limit = self.dynamic.get_multipay_limit()
+            if total_tx % multi_limit == 0:
+                transactions = round(total_tx / multi_limit)
+                transaction_fees = int(transactions * (self.config.multi_fee * self.config.atomic))
+            elif total_tx % multi_limit == 1:
+                multi_transactions = round(total_tx / multi_limit)
+                transaction_fees = int(((multi_transactions * (self.config.multi_fee * self.config.atomic)) + self.dynamic.get_dynamic_fee()))
+            else:
+                transactions = round(tx_count // multi_limit) + 1
+                transaction_fees = int(transactions * (self.config.multi_fee * self.config.atomic))
+        else:
+            transaction_fees = int(total_tx * self.dynamic.get_dynamic_fee()))
+        return transaction_fees
+        
     
     def stage_delegate_payments(self):
         pass
