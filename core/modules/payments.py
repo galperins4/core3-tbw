8eabb26fbea3567f5af837d6e9c2b067b2f647c9
galperins4 <galperins4@gmail.com>
2021-11-26 16:43:43 -0600

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -4,3 +4,9 @@ class Payments:
         self.sql = sql
         self.dynamic = dynamic
         self.utility = utility
+
+        
+    def get_nonce(self):
+        client = self.utility.get_client()
+        n = client.wallets.get(self.config.delegate)
+        return int(n['data']['nonce'])
