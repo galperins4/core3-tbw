19414669bf594f7e9ecd7fc83a536c0a2d27b0a0
galperins4 <galperins4@gmail.com>
2021-11-26 17:42:33 -0600

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -9,48 +9,56 @@ from utility.utility import Utility
 import time
 
 
+def chunks(l, n):
+    # For item i in a range that is a length of l
+    for i in range(0, len(l), n):
+        # Create an index range for l of n items:
+        yield l[i:i+n]
+
+
 def process_multi_payments(payment, unprocessed, dynamic):
     print("Multi Payment")
     print(unprocessed)
     signed_tx = []
-    check = {}       
+    check = {} 
+    request_limit = dynamic.get_tx_request_limit()
+    multi_limit = dynamic.get_multipay_limit()
    
-      
-    # set max multipayment
-    max_tx = dynamic.get_multipay_limit()
-    # hard code multipay for test
-    #max_tx = 6
-    unprocessed_pay = snekdb.stagedArkPayment(multi=data.multi).fetchall()
-    if len(unprocessed_pay) == 1:
-        share()
-    elif unprocessed_pay:
-        temp_multi_chunk = list(chunks(unprocessed_pay, max_tx))
-        # remove any items over tax_tx_limit
-        multi_chunk = temp_multi_chunk[:max_tx_limit]
-        nonce = int(get_nonce() + 1)
+    if len(unprocessed) == 1:
+        process_standard_payments(payments, unprocessed, dynamic, config, exchange)
+    else:
+        temp_multi_chunk = list(chunks(unprocessed, multi_limit))
+        # remove any items over request_tx_limit
+        multi_chunk = temp_multi_chunk[:request_limit]
+        nonce = payment.get_nonce() + 1
+        
         for i in multi_chunk:
             if len(i) > 1:
                 unique_rowid = [y[0] for y in i]
-                tx = build_multi_transaction(i, str(nonce))
+                tx = payment.build_multi_transaction(i, str(nonce))
                 check[tx['id']] = unique_rowid
                 signed_tx.append(tx)
                 nonce += 1        
-        accepted = broadcast_multi(signed_tx)
+        
+        accepted = payment.broadcast_multi(signed_tx)
+        
         #check for accepted and non-accepted transactions
-        for k,v in check.items():
+        for k, v in check.items():
             if k in accepted:
                 # mark all accepted records complete
-                snekdb.processStagedPayment(v)
+                # snekdb.processStagedPayment(v)
+                sql.open_connection()
+                sql.process_staged_payment(v)
+                sql.close_connection()
             else:
                 #delete all transaction records with relevant multipay txid
-                snekdb.deleteTransactionRecord(k) 
+                self.sql.open_connection()
+                self.sql.delete_transaction_record(k)
+                self.sql.close_connection()
+                # snekdb.deleteTransactionRecord(k) 
 
         # payment run complete
         print('Payment Run Completed!')
-        # sleep 3 minutes between tx blasts
-        time.sleep(300)
-    else:
-        time.sleep(300)
     
 
 def process_standard_payments(payment, unprocessed, dynamic, config, exchange):
@@ -129,7 +137,7 @@ if __name__ == '__main__':
         if config.multi == "Y":
             unprocessed = sql.get_staged_payment(multi=config.multi).fetchall()
             sql.close_connection()
-            process_multi_payments(payments, unprocessed, dynamic)
+            process_multi_payments(payment, unprocessed, dynamic, config, exchange)
         else:
             unprocessed = sql.get_staged_payment(dynamic.get_tx_request_limit()).fetchall()
             sql.close_connection()
