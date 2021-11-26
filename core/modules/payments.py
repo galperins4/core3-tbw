d4e66e006130678ab983cc7ec292a86fe49bcbb9
galperins4 <galperins4@gmail.com>
2021-11-26 16:58:05 -0600

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -1,3 +1,6 @@
+from crypto.transactions.builder.transfer import Transfer
+from crypto.transactions.builder.multi_payment import MultiPayment
+
 class Payments:
     def __init__(self, config, sql, dynamic, utility):
         self.config = config
@@ -10,3 +13,18 @@ class Payments:
         client = self.utility.get_client()
         n = client.wallets.get(self.config.delegate)
         return int(n['data']['nonce'])
+
+    
+    def build_transfer_transaction(address, amount, vendor, fee, pp, sp, nonce):
+        # python3 crypto version    
+        transaction = Transfer(recipientId=address, amount=amount, vendorField=vendor, fee=fee)
+        transaction.set_nonce(int(nonce))
+        transaction.schnorr_sign(pp)
+
+        if sp == 'None':
+            sp = None
+        if sp is not None:
+            transaction.second_sign(sp)
+
+        transaction_dict = transaction.to_dict()
+        return transaction_dict
