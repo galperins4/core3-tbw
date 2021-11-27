fc7d88885a46301622c84c4538a03841829a1b0f
galperins4 <galperins4@gmail.com>
2021-11-27 09:56:31 -0600

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -79,7 +79,8 @@ if __name__ == '__main__':
     Initialize(config, database, sql)
     
     # update all voter share
-    update_voter_share(sql, config) if config.update_share == "Y"
+    if config.update_share == "Y":
+        update_voter_share(sql, config)
     
     # check if manual pay flag is set
     
