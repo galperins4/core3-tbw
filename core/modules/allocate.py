7a4e702237d17f76489cad63c46851b438e9efab
galperins4 <galperins4@gmail.com>
2022-04-29 14:40:40 -0500

    Update allocate.py

--- core/modules/allocate.py
+++ core/modules/allocate.py
@@ -139,12 +139,17 @@ class Allocate:
         
         for k , v in delegate_unpaid.items():
             self.logger.debug("Delegate {} account reward: {}".format(k, (v / self.atomic)))
+        
+        # get original voter approval balance (without dilution adjustment)
+        res = self.sql.get_all_voters_balance_checkpoint().fetchall()
+        og_voter_approval = sum(i[0] for i in res)
         self.sql.close_connection()
         
                           
         self.logger.info(f"Processed Block: {block[4]}")
         self.logger.info(f"\tVoters processed: {voter_check}")
-        self.logger.info(f"\tTotal Approval: {total_delegate_vote_balance / self.atomic}")
+        self.logger.info(f"\tTotal Approval (Dilution Adjusted): {total_delegate_vote_balance / self.atomic}")
+        self.logger.info(f"\tTotal Approval Original: {og_voter_approval / self.atomic}")
         self.logger.info(f"\tVoters Rewards: {rewards_check / self.atomic}")
         self.logger.info(f"\tDelegate Reward: {delegate_check / self.atomic}")
         self.logger.info(f"\tVoter + Delegate Rewards: {(rewards_check + delegate_check) / self.atomic}")
