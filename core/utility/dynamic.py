1809aa6c99e9a1c79920c3bedf38ded1e369f964
galperins4 <galperins4@gmail.com>
2022-04-29 15:52:02 -0500

    Update dynamic.py

--- core/utility/dynamic.py
+++ core/utility/dynamic.py
@@ -27,7 +27,7 @@ class Dynamic:
          try:
              node_configs = self.client.node.configuration()['data']['transactionPool']['dynamicFees']
              if (node_configs['enabled'] == "False"):
-                 transaction_fee = int(0.1 * atomic)
+                 transaction_fee = int(0.1 * self.config.atomic)
              else:
                  dynamic_offset = node_configs['addonBytes']['multiPayment']
                  fee_multiplier = node_configs['minFeePool']
