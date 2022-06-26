c009b5480cd5ee0e7463b05a85b41a1599caff81
galperins4 <galperins4@gmail.com>
2022-06-26 18:28:59 -0500

    Update dynamic.py

--- core/utility/dynamic.py
+++ core/utility/dynamic.py
@@ -3,25 +3,6 @@ class Dynamic:
         self.client = utility.get_client()
         self.config = config
         
-    '''
-    def get_dynamic_fee(self):        
-        try:
-            node_configs = self.client.node.configuration()['data']['transactionPool']['dynamicFees']
-            if node_configs['enabled'] == "False":
-                transaction_fee = int(0.1 * self.config.atomic)
-            else:
-                dynamic_offset = node_configs['addonBytes']['transfer']
-                fee_multiplier = node_configs['minFeePool']
-                standard_tx = 230
-                v_msg = len(self.config.message)
-                tx_size = standard_tx + v_msg
-                #calculate transaction fee
-                transaction_fee = self.calculate_dynamic_fee(dynamic_offset, tx_size, fee_multiplier)
-        except:
-            transaction_fee = int(0.1 * self.config.atomic)
-
-        return transaction_fee
-    '''
     
     def get_dynamic_fee(self, numtx):
          try:
@@ -47,10 +28,6 @@ class Dynamic:
 
          return transaction_fee
     
-    '''
-    def calculate_dynamic_fee(self, t, s, c):
-        return int((t+s)*c)
-    '''
     
     def calculate_dynamic_fee(self, t, s, c):
          fee = int((t + (round(s/2) + 1)) * c)
