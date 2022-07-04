c8d50879057b0e78376e8e237d253944881bb438
galperins4 <galperins4@gmail.com>
2022-07-04 12:17:21 -0500

    Update sql.py

--- core/utility/sql.py
+++ core/utility/sql.py
@@ -247,7 +247,7 @@ class Sql:
     
     def get_all_voters_balance_checkpoint(self):
         ts = self.cursor.execute("SELECT MAX(timestamp) FROM voters_balance_checkpoint").fetchall()[0][0]
-        return self.cursor.execute(f"SELECT balance FROM voters_balance_checkpoint WHERE timestamp = {ts}")
+        return self.cursor.execute(f"SELECT balance, voting_balance FROM voters_balance_checkpoint WHERE timestamp = {ts}")
     
     def get_all_voters_last_balance(self):
         ts = self.cursor.execute("SELECT MAX(timestamp) FROM voters_balance_checkpoint").fetchall()[0][0]
