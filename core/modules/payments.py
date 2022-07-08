c8cbf826a230ec58d0d3ee7d097aa5ea2a257031
osrn <65973281+osrn@users.noreply.github.com>
2022-07-08 22:53:41 +0300

    refactor vendorField to memo (#30)
    - add fallback for new fix_time config parameter in case not found in config.ini
    - use solar-client 3.0.0 and solar-crypto 3.1.0 from pypi
    
    # stop tbw, pay [, pool]
    # force reinstall both libraries in case they were previously installed from git repo.
    cd ~/core3-tbw
    . .venv/bin/activate
    pip3 install --upgrade --force-reinstall solar-client
    pip3 install --upgrade --force-reinstall solar-crypto
    deactivate
    # start tbw, pay [, pool]

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -20,7 +20,7 @@ class Payments:
 
     def build_transfer_transaction(self, payments, nonce):
         f = self.dynamic.get_dynamic_fee(len(payments))
-        transaction = Transfer(vendorField=self.config.message)
+        transaction = Transfer(memo=self.config.message)
         transaction.set_fee(f)
         transaction.set_nonce(int(nonce))
 
