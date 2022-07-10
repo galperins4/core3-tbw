b5e75ccb597046c98c86a2f7b6099be01ffb6afb
galperins4 <galperins4@gmail.com>
2022-07-10 09:28:34 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -20,9 +20,11 @@ class Payments:
 
     def build_transfer_transaction(self, payments, nonce):
         f = self.dynamic.get_dynamic_fee(len(payments))
-        transaction = Transfer(memo=self.config.message)
+        # transaction = Transfer(memo=self.config.message)
+        transaction = Transfer()
         transaction.set_fee(f)
         transaction.set_nonce(int(nonce))
+        transaction.set_memo(self.config.message)
 
         for i in payments:
             # exchange processing
