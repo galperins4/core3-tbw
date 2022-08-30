b534a64b08be712004be5181ecd26766112b800b
galperins4 <galperins4@gmail.com>
2022-08-29 19:30:03 -0500

    Update configure.py

--- core/config/configure.py
+++ core/config/configure.py
@@ -35,8 +35,8 @@ class Configure:
         self.whitelist_address = c.get('delegate', 'whitelist_address').split(',')
         self.blacklist = c.get('delegate', 'blacklist')
         self.blacklist_address = c.get('delegate', 'blacklist_address').split(',')
-        self.dynamic_blacklist = c.get('delegate', 'dynamic_blacklist')
-        self.dynamic_bl_endpoint = c.get('delegate', 'dynamic_bl_endpoint')
+        self.dynamic_blacklist = c.get('delegate', 'dynamic_blacklist', , fallback='N')
+        self.dynamic_bl_endpoint = c.get('delegate', 'dynamic_bl_endpoint', fallback='https://example.com')
 
     def payment(self, c):
         self.fix_time = c.get('payment', 'fix_time', fallback='N')
