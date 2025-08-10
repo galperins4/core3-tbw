e28917cd05078637a85347b8563d255ca8665669
galperins4 <galperins4@gmail.com>
2025-08-10 14:05:41 -0500

    Update database.py

--- core/utility/database.py
+++ core/utility/database.py
@@ -36,7 +36,7 @@ class Database:
         print(self.public_key)
         try:
             return self.cursor.execute(f"""SELECT "address" FROM wallets WHERE 
-            "public_key" = 'self.public_key'""".fetchall())
+            "public_key" = 'self.public_key'""".fetchone())
         except Exception as e:
             print(e)
             
