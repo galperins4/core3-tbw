22723344e5b0e41d3c080e7df9d4d2e11aa78aa1
galperins4 <galperins4@gmail.com>
2025-08-14 17:58:02 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -33,7 +33,7 @@ class Payments:
     def build_transfer_transaction(self, address, amount, vendor, fee, nonce):
         # python3 crypto version    
         #transaction = TransferBuilder(recipientId=address, amount=amount, vendorField=vendor, fee=fee)
-        print(address, amount/self.config.atomic, vendor, fee/self.config.atomic, nonce)
+        print(address, amount, vendor, fee, nonce)
         quit()
         transaction = TransferBuilder.new()
         transaction.to(address)
