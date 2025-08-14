870a652a1259188537b899b6e554479340c029d3
galperins4 <galperins4@gmail.com>
2025-08-14 18:12:35 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -81,8 +81,6 @@ class Payments:
     
     def broadcast_standard(self, tx):
         # broadcast to relay
-        print("tx",tx)
-        quit()
         try:
             transaction = self.pool_client.transactions.create(tx)
             print(transaction)
