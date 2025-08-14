facc903701c910cd4ccb3ff636f0c4e4aea20c8f
galperins4 <galperins4@gmail.com>
2025-08-14 18:39:30 -0500

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -29,7 +29,7 @@ def process_multi_payments(payment, unprocessed, dynamic, config, exchange, sql)
         temp_multi_chunk = list(chunks(unprocessed, multi_limit))
         # remove any items over request_tx_limit
         multi_chunk = temp_multi_chunk[:request_limit]
-        nonce = payment.get_nonce() + 1
+        nonce = payment.get_nonce()
         
         for i in multi_chunk:
             if len(i) > 1:
