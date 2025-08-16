e1367d1f83aec80b487445dba9dce3c0d9ffbeeb
galperins4 <galperins4@gmail.com>
2025-08-15 20:33:51 -0500

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -40,9 +40,8 @@ def process_multi_payments(payment, unprocessed, dynamic, config, exchange, sql)
                 nonce += 1        
         
         accepted = payment.broadcast_multi(signed_tx)
-        # temp disable this until response codes are fixed
-        #check for accepted and non-accepted transactions
-        
+        '''
+        #check for accepted and non-accepted transactions        
         for k, v in check.items():
             if k in accepted:
                 # mark all accepted records complete
@@ -55,7 +54,7 @@ def process_multi_payments(payment, unprocessed, dynamic, config, exchange, sql)
                 sql.open_connection()
                 sql.delete_transaction_record(k)
                 sql.close_connection()
-        
+        '''
         # payment run complete
         print('Payment Run Completed!')
     
@@ -90,8 +89,8 @@ def process_standard_payments(payment, unprocessed, dynamic, config, exchange, s
     
     accepted = payment.broadcast_standard(signed_tx)
     print(accepted)
-    # temp disable until response is fixed
-    '''
+    
+    
     for_removal = payment.non_accept_check(check, accepted)
             
     # remove non-accepted transactions from being marked as completed
@@ -99,7 +98,7 @@ def process_standard_payments(payment, unprocessed, dynamic, config, exchange, s
         for i in for_removal:
             print("Removing RowId: ", i)
             unique_rowid.remove(i)
-    '''             
+                 
     quit()
     sql.open_connection()
     sql.process_staged_payment(unique_rowid)
