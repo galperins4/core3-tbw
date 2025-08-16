c06948a0984836a5f7cecc070a7fe6f6a1fdde60
galperins4 <galperins4@gmail.com>
2025-08-15 20:23:12 -0500

    Update tbw.py

--- core/tbw.py
+++ core/tbw.py
@@ -179,6 +179,7 @@ if __name__ == '__main__':
                 s = Stage(config, dynamic, sql, unpaid_voters, unpaid_delegate)
         
             # pause betweeen blocks
+            time.sleep(35)
  
         print("End Script - Looping")
         time.sleep(1200)
