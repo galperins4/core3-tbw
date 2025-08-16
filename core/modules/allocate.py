111ac0ec2f50021314a47b67f447e572f275e9b2
galperins4 <galperins4@gmail.com>
2025-08-16 16:04:22 -0500

    Update allocate.py

--- core/modules/allocate.py
+++ core/modules/allocate.py
@@ -23,8 +23,6 @@ class Allocate:
         # counter = voters_data['meta']['pageCount']
         while pages == counter:
             c = client.validators.voters(validator_id=self.config.delegate, page=pages)
-            print(len(c['data']))
-            quit()
             for j in c['data']:
                 initial_voters[j['address']] = int(j['balance'])
                 val = [j['address'], j['publicKey']]
