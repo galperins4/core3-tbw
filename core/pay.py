5fe2044807406695709dc5db6d44eec4e88e0925
galperins4 <galperins4@gmail.com>
2025-08-15 20:22:20 -0500

    Update pay.py

--- core/pay.py
+++ core/pay.py
@@ -83,6 +83,10 @@ def process_standard_payments(payment, unprocessed, dynamic, config, exchange, s
         signed_tx.append(tx_hex)
         temp_nonce += 1 
                      
+    print("unique_row", unique_rowid, len(unique_rowid))
+    print("check", check, len(check))
+    quit()
+    
     accepted = payment.broadcast_standard(signed_tx)
     # temp disable until response is fixed
     '''
