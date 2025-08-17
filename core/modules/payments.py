8a3f013342631722f09db10c039ee2b228538cae
galperins4 <galperins4@gmail.com>
2025-08-16 21:03:47 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -56,6 +56,7 @@ class Payments:
         fee = self.dynamic.get_dynamic_fee_multi(len(payments))
         transaction = MultipaymentBuilder.new()
         print(fee)
+        quit()
 
         for i in payments:
             # exchange processing
