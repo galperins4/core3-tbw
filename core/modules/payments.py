5aeadd3af633f05752cc737b9e20d983577de707
galperins4 <galperins4@gmail.com>
2025-08-17 16:40:18 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -110,7 +110,7 @@ class Payments:
                 hash = i['hash']
                 count = 0
                 for j in i['pay']:
-                    print(j[0][0]), j[1][0])
+                    print(j[0][0], j[1][0])
                     quit()
                     to = j[0][count]
                     value = int(j[1][count] / self.config.offset)
