b654e939804bba87ea179752476e260a1c8f1d4e
galperins4 <galperins4@gmail.com>
2025-08-17 16:43:28 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -111,7 +111,7 @@ class Payments:
                 count = 0
                 for j in i['pay']:
                     print(j[0])
-                    print(j[1])
+                    print(j[5])
                     quit()
                     to = j[0][count]
                     value = int(j[1][count] / self.config.offset)
