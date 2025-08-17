ec0d879b2f88fe994a9541c5c1e322a7aea61022
galperins4 <galperins4@gmail.com>
2025-08-16 20:05:52 -0500

    Update dynamic.py

--- core/utility/dynamic.py
+++ core/utility/dynamic.py
@@ -10,7 +10,7 @@ class Dynamic:
             fees = {"gas_price": node_configs['constants']['gas']['minimumGasPrice'],
                     "gas_limit": node_configs['constants']['gas']['minimumGasLimit']}
 
-            fees["tx_fee"] = (fees['gas_price']*fees['gas_limit'])
+            fees["tx_fee"] = (fees['gas_price'] * fees['gas_limit'])
         except:
             # HARD CODED
             fees = {"gas_price": 5000000000, "gas_limit": 21000, "tx_fee": 105000000000000} 
