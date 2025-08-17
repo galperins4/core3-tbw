767a3f1d5da95ad61c861ba2df1c2914dd71d3c7
galperins4 <galperins4@gmail.com>
2025-08-17 16:46:34 -0500

    Update payments.py

--- core/modules/payments.py
+++ core/modules/payments.py
@@ -109,6 +109,8 @@ class Payments:
                 records = []
                 hash = i['hash']
                 count = 0
+                print(i['pay'])
+                quit()
                 for j in i['pay']:
                     print(j)
                     quit()
